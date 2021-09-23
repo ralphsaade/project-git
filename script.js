50d50452013548a406ab0abdf86d50a1206650c2
ralphsaade <86687858+ralphsaade@users.noreply.github.com>
2021-09-23 16:34:28 +0100

    keep on going ( you can do it )

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-function compute()
-{
+function compute() {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML = `<b>${principal}</b> if you deposit ${principal.getElementById()}`;
-    document.getElementById("result").innerHTML = `<b>${rate}</b> at interest rate of ${updateRate.getElementById()}`;
-    document.getElementById("result").innerHTML = `<b>${interest}</b> You will receive an amount of ${rateval.getFullYear()}`;
-    document.getElementById("result").innerHTML = `<b>${years}</b> in the year ${year.getElementById()}`;
-
-}    
-
-function updateRate() 
-{
+    var year = new Date().getFullYear()+parseInt(years)
+    result = document.getElementById("result");
+    result.innerHTML=
+    "If you deposite" + " " + (principal) + " <br> " + 
+    "At an interest rate of" + " " + (rate) + " <br> "+ 
+    "You will recieve an amount of" + " " + (principal * years * rate / 100) + " <br>" + 
+    "In the year" + " " + (year);
+}
+function updateRate() {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
-    document.getElementById("slider").onchange = rangeSliderVal;
+}
+function validation() {
+    var num = /^[0]+$/;
+    if (principal.value.match(num))  {
+    alert("Enter Positive Number");
+    }else{null;
+         }
 }
